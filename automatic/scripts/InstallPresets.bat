ffc96bb06280041ee5bc73ed56d74046c8c0fb3e
Stephen White <iamsilk3495@gmail.com>
2022-02-22 13:53:11 -0400

    Move script-related files into automatic folder

--- automatic/scripts/InstallPresets.bat
+++ automatic/scripts/InstallPresets.bat
@@ -45,7 +45,7 @@ taskkill /T /IM %LAUNCHER_PROCESS% >nul 2>nul
 echo ------------------------------------------------------------
 echo Downloading mod presets list
 
-set REMOTE_GITHUB_REPO_URL=https://raw.githubusercontent.com/TacticalGaming-net/ARMA-Mod-Presets/main
+set REMOTE_GITHUB_REPO_URL=https://raw.githubusercontent.com/TacticalGaming-net/ARMA-Mod-Presets/main/automatic
 
 set REMOTE_PRESETS_LIST_URL=%REMOTE_GITHUB_REPO_URL%/enabled_presets.txt
 set REMOTE_PRESETS_URL=%REMOTE_GITHUB_REPO_URL%/presets
